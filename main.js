100fb5527c52977f075516ad77d379439cfdd4ef
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-19 16:34:30 -0500

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,48 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
 
+  const modal = document.querySelector("#modal")
+  const hearts = document.querySelectorAll("span.like-glyph")
+
+  modal.hidden = true
+
+  for (const heart of hearts) {
+    heart.addEventListener("click", (event) => {
+      // make a server call
+      // promises have the `.then()` function
+      // TWO of them that we use:
+      // 1. is to take the response, jsonify it.
+      // 2. take the jsonified response, do something with it
+      mimicServerCall() // return a promise......this is a fake fetch so we can just go into our second `.then`
+      .then(() => {
+        // when successful, change the heart
+        // if it's empty, make it full, add the `.activated-heart` class
+        // else if it's full, make it empty, remove `.activated-heart` class
+        if (heart.innerHTML == EMPTY_HEART){
+          heart.innerHTML = FULL_HEART
+          heart.className = "activated-heart"
+        } else {
+          heart.innerHTML = EMPTY_HEART
+          heart.className = "like-glyph"
+        }
+      })
+      .catch(error => {
+        modal.hidden = false
+        const modalMessage = document.querySelector("#modal-message")
+        modalMessage.innerText = error
+        setTimeout(() => {
+          // do this during the timeout
+          modal.hidden = true
+        }, 5000) // times out after 5 seconds
+      })
+    })
+  }
+
+
+  // DOM Content Loaded
+})
 
 
 //------------------------------------------------------------------------------
